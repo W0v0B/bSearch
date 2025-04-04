ddf81b9ae209c59e32df49d6511a1057e91bc260
W0v0B <2081969239@qq.com>
2025-04-05 03:42:50 +0800

    Install necessary dependence 1. Vue3     - fuse.js # Fuzzy search     - @vueuse/core # Vue Composition API 2. Rust     - global-hotkey # Global hotkeys     - walkdir # File system traversal     - fuzzy-matcher # Fuzzy matching

--- src/App.vue
+++ src/App.vue
@@ -2,20 +2,7 @@
 </script>
 
 <template>
-  <main class="container">
-  </main>
 </template>
 
-<style scoped>
-</style>
 <style>
-.container {
-  margin: 0;
-  padding-top: 10vh;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  text-align: center;
-}
-
 </style>
