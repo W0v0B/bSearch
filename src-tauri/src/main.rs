e891eaf48762de7e4d7c329a6fc37064f1043122
W0v0B <2081969239@qq.com>
2025-04-11 00:13:48 +0800

    add icon

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,5 +1,4 @@
 // src-tauri/src/main.rs
-// Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 use bsearch_lib::run;
